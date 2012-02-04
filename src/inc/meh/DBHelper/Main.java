75f913e6935d34b4a0cd6bd997ee986c248c7380
liamehinc <sysproxy@hotmail.com>
2012-02-03 19:54:15 -0800

    added basic menu functionality  (under res\menu\mainmenu.xml and lines 351 - 362 in main.java)
    will serve as the basis for using menu options
    
    Signed-off-by: liamehinc <sysproxy@hotmail.com>

--- src/inc/meh/DBHelper/Main.java
+++ src/inc/meh/DBHelper/Main.java
@@ -9,6 +9,9 @@ import android.location.*;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -345,6 +348,19 @@ public class Main extends Activity {
 
 	// Close onCreate
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.mainmenu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Toast.makeText(this, "Just a test", Toast.LENGTH_SHORT).show();
+		return true;
+	}
+	
 	@Override
 	public void onSaveInstanceState(Bundle outState)
 	{
