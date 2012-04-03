024fed7fa71154c886bfa9c458977b043a5162a3
okkam1 <okkam1@hotmail.com>
2012-04-03 16:26:36 -0700

    removing comments about reduced accuracy when running in the background from the exit confirmation dialog
    Signed-off-by: okkam1 <okkam1@hotmail.com>

--- src/inc/meh/MileageTracker/Main.java
+++ src/inc/meh/MileageTracker/Main.java
@@ -148,7 +148,7 @@ public class Main extends Activity {
 		{
 
 			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			builder.setMessage("Are you sure you want to exit while a trip is running?\n\nTo use other applications, please use the Home button.  You can still track but the accuracy will be reduced when running in the background.")
+			builder.setMessage("Are you sure you want to exit while a trip is running?\n\nTo use other applications, please use the Home button.  You can still track when running in the background.")
 			       .setCancelable(false)
 			       .setTitle("Confirm Exit")
 			       .setPositiveButton("Exit and Stop Trip", new DialogInterface.OnClickListener() {
